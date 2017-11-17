e87697f0af1db17f463b6badf0fd09637720d863
guoxiaolu <guoxiaolu06@126.com>
2017-11-17 15:20:22 +0800

    modify evaluation

--- evaluation.py
+++ evaluation.py
@@ -11,22 +11,22 @@ from resnet_101 import resnet101_model, Scale
 import cv2
 from imagenet_tool import id_to_synset, synset_to_id
 
-val_gt = '/media/wac/backup/imagenet/devkit/data/ILSVRC2015_clsloc_validation_ground_truth.txt'
-val_file = '/media/wac/backup/imagenet/ILSVRC/ImageSets/CLS-LOC/val.txt'
-val_path = '/media/wac/backup/imagenet/ILSVRC/Data/CLS-LOC/val_dir'
-categories = './categories.txt'
-
-f = open(categories, mode='r')
-classes = [line.strip() for line in f.readlines()]
-
-f_file = open(val_file, mode='r')
-f_gt = open(val_gt, mode='r')
-lines_file = f_file.readlines()
-lines_gt = f_gt.readlines()
-
-fname = [line.strip().split(' ')[0] for line in lines_file]
-gt = [int(line.strip())for line in lines_gt]
-val_dict = dict(zip(fname, gt))
+# val_gt = '/media/wac/backup/imagenet/devkit/data/ILSVRC2015_clsloc_validation_ground_truth.txt'
+# val_file = '/media/wac/backup/imagenet/ILSVRC/ImageSets/CLS-LOC/val.txt'
+# val_path = '/media/wac/backup/imagenet/ILSVRC/Data/CLS-LOC/val_dir'
+# categories = './categories.txt'
+#
+# f = open(categories, mode='r')
+# classes = [line.strip() for line in f.readlines()]
+#
+# f_file = open(val_file, mode='r')
+# f_gt = open(val_gt, mode='r')
+# lines_file = f_file.readlines()
+# lines_gt = f_gt.readlines()
+#
+# fname = [line.strip().split(' ')[0] for line in lines_file]
+# gt = [int(line.strip())for line in lines_gt]
+# val_dict = dict(zip(fname, gt))
 #
 #
 # weights_path = './model/weights.00298.hdf5'
@@ -54,29 +54,31 @@ val_dict = dict(zip(fname, gt))
 # score = model.evaluate_generator(val_generator, val_generator.n)
 # print score
 
+val_path = '/media/wac/backup/imagenet/ILSVRC/Data/CLS-LOC/val_dir'
+gt_list = os.listdir(val_path)
 
-
-
-
-
+model = resnet50.ResNet50(weights='imagenet')
 top1 = 0
 top5 = 0
-for i,name in enumerate(fname):
-    model = resnet50.ResNet50(weights='imagenet')
+total = 0
+for gt_name in gt_list:
+    gt_path = os.path.join(val_path, gt_name)
+    img_list = os.listdir(gt_path)
+    for name in img_list:
+        img_path = os.path.join(gt_path, name)
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = imagenet_utils.preprocess_input(x)
 
-    img_path = os.path.join(val_path, name)
-    img = image.load_img(img_path, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = imagenet_utils.preprocess_input(x)
-
-    preds = model.predict(x)
-    # decode the results into a list of tuples (class, description, probability)
-    # (one such list for each sample in the batch)
-    result = imagenet_utils.decode_predictions(preds, top=5)[0]
-    encode_result = [v[0] for v in result]
-    if encode_result == gt[i]:
-        top1 += 1
-    if gt[i] in encode_result:
-        top5 += 1
-    print i, top1, top5
+        preds = model.predict(x)
+        # decode the results into a list of tuples (class, description, probability)
+        # (one such list for each sample in the batch)
+        result = imagenet_utils.decode_predictions(preds, top=5)[0]
+        encode_result = [v[0] for v in result]
+        if encode_result[0] == gt_name:
+            top1 += 1
+        if gt_name in encode_result:
+            top5 += 1
+        total += 1
+        print total, gt_name, top1, top5
